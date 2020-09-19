708d5a6035e8d981afa11ac413f51222703168a0
Bagh-eera <67158289+Bagh-eera@users.noreply.github.com>
2020-09-19 22:21:09 +0530

    Update 146-lru-cache.cpp

--- 146-lru-cache.cpp
+++ 146-lru-cache.cpp
@@ -1,1 +1,53 @@
 
+class LRUCache {
+public:
+    list<int> lru;
+    map<int, pair<int, list<int>::iterator> > theMap;
+    int max_capacity;
+    
+    LRUCache(int capacity) {
+        max_capacity = capacity;
+        lru.clear();
+        theMap.clear();
+    }
+    
+    int get(int key) {
+        int res;
+        if( theMap.find(key) == theMap.end())
+            return -1;
+        else{
+            // Move the kv pair to the beginning of LRU list
+            put(key, theMap[key].first);
+            res = theMap[key].first;
+        }
+        return res;
+    }
+    
+    void put(int key, int value) {
+        if(theMap.find(key) != theMap.end()){
+            //list erase function -->  <list_name>.erase(<list iterator at position>)
+            lru.erase( theMap[key].second);
+            // lru.push_front(key);
+        }
+        else if(lru.size() == max_capacity){
+            theMap.erase(lru.back());
+            lru.pop_back();
+        }
+        // Add element into LRU
+        lru.push_front(key);
+        // Add corresponding entry in hashtable
+        
+        pair<int, list<int>::iterator> new_entry;
+        new_entry.first = value;
+        new_entry.second = lru.begin();
+        theMap[key] = new_entry;
+        
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
